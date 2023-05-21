e7677e95d6aa407f46a2126a071f4a38b0c985e6
fb0801 <fbhatti0801@gmail.com>
2023-05-21 19:26:01 +0100

    building 2

--- urlshort/urlshortner/shortner/views.py
+++ urlshort/urlshortner/shortner/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import uuid
 from .models import Url
 from django.http import HttpResponse
@@ -15,3 +15,8 @@ def create(request):
         new_url = Url(link=link, uuid=uid)
         new_url.save()
         return HttpResponse(uid)
+    
+
+def go(request,pk):
+    url_details = Url.object.get(uuid=pk)
+    return redirect('https://'+url_details.link)
