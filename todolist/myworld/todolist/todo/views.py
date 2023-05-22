2c9f59e8dcfe095ef7bb58092f4093a9a460ab00
fb0801 <fbhatti0801@gmail.com>
2023-05-22 18:21:05 +0100

    building 3 and setup todo

--- todolist/myworld/todolist/todo/views.py
+++ todolist/myworld/todolist/todo/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    #adding todo object
+    todo= Todo.object.all()
+    if request.method =='POST':
+        new_todo = Todo(
+            title = request.POST['title']
+        )
+        new_todo.save()
+        return redirect('/')
+    
+    return render(request, 'index.html', {'todos':todo})
+
+
+def delete(request, pk):
+    #delete todo object
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect('/') #return user to homepage
